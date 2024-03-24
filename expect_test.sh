5981132aeaa6ae3daa93412d6436e2d91245af29
josephyuzb <56053447@qq.com>
2024-03-24 22:55:15 +0800

    Update expect_test.sh
    added following commands if the amdgpu driver is not there or editing /etc/default/grub file then reboot

--- expect_test.sh
+++ expect_test.sh
@@ -4,20 +4,24 @@ spawn ./amdgpu_ls.sh
 expect {
     -re "amdgpu" {
     	send_user "PLS disable amdgpu module before updating Firmware !!!\n"
-        exit
+  	exit
     }
 }
 # list the firmware version to see if they are up to date.
-spawn ./amdgpuvbflash -i
-spawn ./amdgpuvbflash -i >>listfile 2>&1 
-spawn grep -o "113-D67301V-073" listfile | wc -l
+spawn ./amdvbflash -i
+spawn ./amdvbflash -i >>listfile.txt 2>&1
+spawn grep -o "113-D67301V-073" listfile.txt 
 expect {
 	-re "8"{
          send_user "firmware updated to 8 MI210 cards,reboot to confirm it!!!"
 	}
-	#if firmware are not update,do this updating to 073 version
-	spawn ./amdvbflash -pa -padevid=740f -fv ../113-D67301V-073
-        send_user "updating 073 firmware to 8 cards wait until it finished!!!!"
+	eof
+	{
+		#if firmware are not update,do this updating to 073 version
+		spawn ./amdvbflash -pa -padevid=740f -fv ../113-D67301V-073
+        	send_user "updating 073 firmware to 8 cards wait until it finished!!!!"
+	}
 }
 
-expect off
+#interact
+#expect eof
